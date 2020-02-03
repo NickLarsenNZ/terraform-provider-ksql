bb07a15a194184fe374422caf3a7ea83a6df2ab8
Nick <NickLarsenNZ@users.noreply.github.com>
2020-02-03 19:44:55 +1300

    Add basicAuth provider schema, and log #9

--- ksql/provider.go
+++ ksql/provider.go
@@ -16,6 +16,24 @@ func Provider() terraform.ResourceProvider {
 				Optional:    true,
 				DefaultFunc: schema.EnvDefaultFunc("KSQL_SERVER_URL", "http://localhost:8088"),
 			},
+			"basicAuth": {
+				Type:     schema.TypeSet,
+				Optional: true,
+				MaxItems: 1,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"username": {
+							Type:     schema.TypeString,
+							Required: true,
+						},
+						"password": {
+							Type:      schema.TypeString,
+							Sensitive: true,
+							Required:  true,
+						},
+					},
+				},
+			},
 		},
 
 		ConfigureFunc: providerConfigure,
@@ -29,6 +47,11 @@ func Provider() terraform.ResourceProvider {
 func providerConfigure(d *schema.ResourceData) (interface{}, error) {
 	n := d.Get("url").(string)
 	log.Printf("[INFO] configuring Provider %s", n)
+
+	if _, ok := d.GetOk("basicAuth"); ok {
+		log.Printf("[WARN] basicAuth configured, but not used: NOT IMPLEMENTED")
+	}
+
 	c := ksql.NewClient(n)
 
 	return c, nil
